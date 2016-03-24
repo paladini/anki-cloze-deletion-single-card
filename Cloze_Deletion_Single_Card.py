873151137e531719701ad535352c634ab7db972f
Fernando Paladini <fnpaladini@gmail.com>
2016-03-24 11:16:11 -0300

    Fix problem with tooltip import.

--- Cloze_Deletion_Single_Card.py
+++ Cloze_Deletion_Single_Card.py
@@ -7,14 +7,14 @@
 #
 from anki.hooks import wrap
 from aqt.editor import Editor
+from aqt.utils import showInfo, tooltip
 import re
 
 def newOnCloze(self):
     # check that the model is set up for cloze deletion
     if not re.search('{{(.*:)*cloze:',self.note.model()['tmpls'][0]['qfmt']):
         if self.addMode:
-            tooltip(_("Warning, cloze deletions will not work until "
-            "you switch the type at the top to Cloze."))
+            tooltip(_("Warning, cloze deletions will not work until you switch the type at the top to Cloze."))
         else:
             showInfo(_("""\
 To make a cloze deletion on an existing note, you need to change it \
